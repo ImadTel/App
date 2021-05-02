11597f8c9718ca40a37d6851afbbb493b3ae93d5
Imad Tellache <imad.tel.imad@gmail.com>
2021-05-02 15:36:29 +0200

    urls.py changed

--- mysite/urls.py
+++ mysite/urls.py
@@ -19,6 +19,7 @@ from django.urls import path,include
 
 urlpatterns = [
      path('polls/', include('polls.urls')),
-    path('admin/', admin.site.urls),
+     path('admin/', admin.site.urls),
+     path('yes/', admin.site.urls),
    
 ]
