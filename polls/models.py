4ab3c93a5a201022bd8e97778b7f12248ba3a29c
Imad Tellache <imad.tel.imad@gmail.com>
2021-05-02 15:07:29 +0200

    models changed i added category table

--- polls/models.py
+++ polls/models.py
@@ -9,10 +9,14 @@ class Person(models.Model):
     birthDate=models.DateField(null=True,blank=True)
 
     def __str__(self):
-        self.firstName + '   ' + self.lastName
+       return self.firstName + '   ' + self.lastName
 
     class Meta:
         verbose_name = 'Persons'
         verbose_name_plural = 'persons'
 
 
+class Category(models.Model):
+    Category = models.CharField(max_length=50)
+    def __str__(self):
+        return self.Category
